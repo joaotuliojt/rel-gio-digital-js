b29862b62045dfd1db3156aa08a5ae3bf09352cb
joaotuliojt <joaotuliosoares@hotmail.com>
2021-07-14 10:32:11 -0300

    Primeira Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+setInterval(changeHour,1000);
+
+function changeHour(){
+    const date = new Date();
+    const hour = formate(date.getHours());
+    const minutes = formate(date.getMinutes());
+    const seconds = formate(date.getSeconds());
+    const hours = concatenate(hour,minutes,seconds)
+    let greeting = getG(hour);
+    setHour(greeting,hours);
+}
+
+function formate(num){
+    return num >= 10 ? num : `0${num}`
+}
+function concatenate(hour,minutes,seconds){
+    return `${hour}: ${minutes}: ${seconds}`
+}
+function getG(hour){
+    if(hour < 12){
+        return "Bom dia!"
+    }else if(hour >= 12){
+        return "Boa tarde!"
+    }else if(hour >= 18){
+        return "Boa noite!"
+    }
+}
+function setHour(greeting,hours){
+    const h1Greetings = document.querySelector('#greetings')
+    const schedule = document.querySelector('#schedule')
+    h1Greetings.innerHTML = greeting;
+    schedule.innerHTML = hours;
+}
+
+function changeBg(){
+    const body = document.querySelector('body')
+    body.classList.toggle('bg-light');
+}
